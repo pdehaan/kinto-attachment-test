06768b1d0a8e9faeece2b594e8bd11f27cfa21ad
Peter deHaan <peter@deseloper.com>
2021-11-24 09:29:37 -0800

    FIRST BLOOD

---
+++ run.sh
@@ -0,0 +1,10 @@
+python3 upload.py \
+  --server=https://demo.kinto-storage.org/ \
+  --bucket='f8ba1b65-d87b-f57a-8e22-0f731d957e68' \
+  --collection=test \
+  --force \
+  --debug \
+  --verbose \
+  testSuggest_queries.json
+
+  # --auth "admin:topsecret" \
